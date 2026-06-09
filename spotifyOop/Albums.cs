f0a0e5deff032c14303bebf47396d72ee66cd1ae
sennaTia <1214998@student.roc-nijmegen.nl>
2026-06-09 11:32:46 +0200

    Partly working

---
+++ spotifyOop/Albums.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace spotifyOop
+{
+    internal class Albums(String name, List<String> genres)
+    {
+        public List<Number> numbers = [];
+        public String Name { get; } = name;
+        public List<String> Genre { get; } = genres;
+
+        
+
+
+
+
+
+        //public List<Number> allNumbers { get; } = new List<Number>();
+
+
+    }
+}
