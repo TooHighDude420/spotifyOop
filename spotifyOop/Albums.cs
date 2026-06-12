3eaa15a5db6aba419e452963a382e0e0f4674be8
TooHighDude420 <nataro112@gmail.com>
2026-06-12 11:30:24 +0200

    implemented readonly album

--- spotifyOop/Albums.cs
+++ spotifyOop/Albums.cs
@@ -1,25 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
 namespace spotifyOop
 {
-    internal class Albums(String name, List<String> genres)
+    internal class Albums(String name, List<String> genres, List<Number> numbers)
     {
-        public List<Number> numbers = [];
+        private List<Number> Album = [];
+        public IReadOnlyList<Number> ReadAlbum
+        {
+            get {
+                return this.Album.ToImmutableList(); 
+            }
+        }
+
         public String Name { get; } = name;
         public List<String> Genre { get; } = genres;
-
-        
-
-
-
-
-
-        //public List<Number> allNumbers { get; } = new List<Number>();
-
-
     }
 }
