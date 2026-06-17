94b42e3d83eed9ef9471e2fe3856560646eaf01c
TooHighDude420 <nataro112@gmail.com>
2026-06-17 11:11:14 +0200

    extended numbers

--- spotifyOop/Number.cs
+++ spotifyOop/Number.cs
@@ -6,6 +6,15 @@ namespace spotifyOop
 {
     internal class Number
     {
+        public string Name;
+        public string Genre;
+        public string Artist;
 
+        public Number(string name, string genre, string artist)
+        {
+            this.Name = name;
+            this.Genre = genre;
+            this.Artist = artist;
+        }
     }
 }
